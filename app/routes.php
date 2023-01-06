1c85245ef2231c801db18ed42856c2d52cf22868
Ngo Quoc Dat <taylor@laravel.com>
2023-01-06 23:17:09 +0700

    Update project

--- app/routes.php
+++ app/routes.php
@@ -1,2 +1,13 @@
 <?php
 
+use App\Controllers\HomeController;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
+
+$routes = new RouteCollection();
+
+$routes->add('home', new Route('/', [
+    '_controller' => [new HomeController(), 'index'],
+]));
+
+return $routes;
--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,42 @@
 <?php
 
-require_once 'vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
-$app = new \FPOLYHCM\WEB3014\Application();
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel;
+use Symfony\Component\Routing;
 
-$app->run();
+function render_template(Request $request): Response
+{
+    extract($request->attributes->all(), EXTR_SKIP);
+    ob_start();
+    include sprintf(__DIR__ . '/app/views/%s.php', $_route);
+
+    return new Response(ob_get_clean());
+}
+
+$request = Request::createFromGlobals();
+$routes = include __DIR__ . '/app/routes.php';
+
+$context = new Routing\RequestContext();
+$context->fromRequest($request);
+$matcher = new Routing\Matcher\UrlMatcher($routes, $context);
+
+$controllerResolver = new HttpKernel\Controller\ControllerResolver();
+$argumentResolver = new HttpKernel\Controller\ArgumentResolver();
+
+try {
+    $request->attributes->add($matcher->match($request->getPathInfo()));
+
+    $controller = $controllerResolver->getController($request);
+    $arguments = $argumentResolver->getArguments($request, $controller);
+
+    $response = call_user_func_array($controller, $arguments);
+} catch (Routing\Exception\ResourceNotFoundException $exception) {
+    $response = new Response('Not Found', 404);
+} catch (Exception $exception) {
+    $response = new Response('An error occurred', 500);
+}
+
+$response->send();
