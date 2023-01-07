ec5cc206b653f378743f2b364c56b13f5e0802f9
Ngo Quoc Dat <datlechin@gmail.com>
2023-01-07 19:28:43 +0700

    Update code

--- bootstrap.php
+++ bootstrap.php
@@ -1,8 +1,10 @@
 <?php
 
+use Core\Application;
+
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/app/routes.php';
 
-$app = new \Core\Application();
+$app = new Application();
 
 $app->run();
--- core/Application.php
+++ core/Application.php
@@ -2,6 +2,9 @@
 
 namespace Core;
 
+use Illuminate\Database\Capsule\Manager;
+use Symfony\Component\Dotenv\Dotenv;
+
 class Application
 {
     protected Router $router;
@@ -12,10 +15,39 @@ class Application
     {
         $this->router = new Router();
         $this->request = new Request();
+
+        $this->loadEnvironment();
+        $this->connectToDatabase();
     }
 
     public function run(): void
     {
         echo $this->router->resolve();
+    }
+
+    protected function connectToDatabase(): void
+    {
+        $capsule = new Manager();
+
+        $capsule->addConnection([
+            'driver' => 'mysql',
+            'host' => $_ENV['DB_HOST'],
+            'database' => $_ENV['DB_NAME'],
+            'username' => $_ENV['DB_USERNAME'],
+            'password' => $_ENV['DB_PASSWORD'],
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix' => '',
+        ]);
+
+        $capsule->setAsGlobal();
+
+        $capsule->bootEloquent();
+    }
+
+    protected function loadEnvironment(): void
+    {
+        $dotenv = new Dotenv();
+        $dotenv->load(__DIR__ . '/../.env');
     }
 }
