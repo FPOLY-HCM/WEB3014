e667c5e45b6e52a16818dfed4e685272ead1532d
shibakunkun <nguyendangkhai111@gmail.com>
2023-02-04 15:35:28 +0700

    fix

--- app/Controllers/Auth/LoginController.php
+++ app/Controllers/Auth/LoginController.php
@@ -7,7 +7,6 @@ use Core\View;
 use Core\Request;
 use Core\Auth;
 
-
 class LoginController extends Controller
 {
     public function index()
@@ -15,13 +14,13 @@ class LoginController extends Controller
         return View::render('auth/login');
     }
 
-    public function login(){
+    public function login()
+    {
         $request = new Request();
 
         $email = $request->input('email');
         $password = $request->input('password');
 
-        
         if ((new Auth('account'))->attempt($email, $password)) {
             return redirect('/');
         }
