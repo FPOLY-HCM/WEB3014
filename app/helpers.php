8b0a7cfc91d7288bdb2a2b94df080245a746800b
Ngo Quoc Dat <datlechin@gmail.com>
2023-02-05 22:33:38 +0700

    Add declare strict type

--- app/helpers.php
+++ app/helpers.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Core\View;
 
 if (! function_exists('redirect')) {
--- app/routes.php
+++ app/routes.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use App\Controllers\HomeController;
 use App\Controllers\Auth\LoginController;
 use App\Controllers\Auth\RegisterController;
