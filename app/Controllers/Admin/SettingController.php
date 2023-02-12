921a8fd9bd9728340d1078f9e712ce10c6723665
Ngo Quoc Dat <datlechin@gmail.com>
2023-02-12 18:33:09 +0700

    wip

---
+++ app/Controllers/Admin/SettingController.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controllers\Admin;
+
+use Core\Controller;
+
+class SettingController extends Controller
+{
+    public function index()
+    {
+        return view('admin/settings');
+    }
+    
+    public function store()
+    {
+    }
+}
