cd715744b6fccef9cc67e49515b8572d38817f04
Khải <92205964+dang-khai@users.noreply.github.com>
2023-02-16 12:29:49 +0700

    company detail (#49)
    * company detail
    
    * fix
    
    * fix
    
    * fix

--- app/Controllers/CandidateController.php
+++ app/Controllers/CandidateController.php
@@ -4,13 +4,16 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
+use App\Models\Account;
 use Core\Controller;
 
 class CandidateController extends Controller
 {
     public function index()
     {
-        return view('candidates/index');
+        $accounts = Account::all();
+
+        return view('candidates/index', compact('accounts'));
     }
     
     public function show()
