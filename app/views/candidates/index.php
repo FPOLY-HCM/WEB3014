447df9bbb4c95bbab2c6266f1a02a1e5f05bd20a
shibakunkun <nguyendangkhai111@gmail.com>
2023-02-16 20:08:09 +0700

    candidate_detail

--- app/views/candidates/index.php
+++ app/views/candidates/index.php
@@ -59,25 +59,18 @@ require_once __DIR__ . '/../partials/header.php';
                     <div class="col-xl-3 col-lg-4 col-md-6">
                         <div class="card-grid-2 hover-up">
                             <div class="card-grid-2-image-left">
-                                <div class="card-grid-2-image-rd online"><a href="/candidate/show">
-                                        <figure><img alt="<?= env('APP_NAME') ?>" src="<?= asset('assets/imgs/page/candidates/user1.png') ?>"></figure>
+                                <div class="card-grid-2-image-rd online"><a href="/candidate/show?id=<?= $account->id ?>">
+                                        <figure><img alt="<?= $account->name ?>" src="<?= asset('assets/imgs/page/candidates/user1.png') ?>"></figure>
                                     </a></div>
-                                <div class="card-profile pt-10"><a href="/candidate/show">
-                                        <h5>Robert Fox</h5>
-                                    </a><span class="font-xs color-text-mutted">UI/UX Designer</span>
+                                <div class="card-profile pt-10"><a href="/candidate/show?id=<?= $account->id ?>">
+                                        <h5><?= $account->name ?></h5>
                                     <div class="rate-reviews-small pt-5"><span><img src="<?= asset('assets/imgs/template/icons/star.svg') ?>" alt="<?= env('APP_NAME') ?>"></span><span><img src="<?= asset('assets/imgs/template/icons/star.svg') ?>" alt="<?= env('APP_NAME') ?>"></span><span><img src="<?= asset('assets/imgs/template/icons/star.svg') ?>" alt="<?= env('APP_NAME') ?>"></span><span><img src="<?= asset('assets/imgs/template/icons/star.svg') ?>" alt="<?= env('APP_NAME') ?>"></span><span><img src="<?= asset('assets/imgs/template/icons/star.svg') ?>" alt="<?= env('APP_NAME') ?>"></span><span class="ml-10 color-text-mutted font-xs">(65)</span></div>
                                 </div>
                             </div>
                             <div class="card-block-info">
-                                <p class="font-xs color-text-paragraph-2">| Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero repellendus magni, atque delectus molestias quis?</p>
+                                <p class="font-xs color-text-paragraph-2"><?= $account->bio ?></p>
                                 <div class="card-2-bottom card-2-bottom-candidate mt-30">
                                     <div class="text-start"><a class="btn btn-tags-sm mb-10 mr-5" href="jobs-grid.html">Figma</a><a class="btn btn-tags-sm mb-10 mr-5" href="jobs-grid.html">Adobe XD</a><a class="btn btn-tags-sm mb-10 mr-5" href="jobs-grid.html">PSD</a><a class="btn btn-tags-sm mb-10 mr-5" href="jobs-grid.html">App</a><a class="btn btn-tags-sm mb-10 mr-5" href="jobs-grid.html">Digital</a>
-                                    </div>
-                                </div>
-                                <div class="employers-info align-items-center justify-content-center mt-15">
-                                    <div class="row">
-                                        <div class="col-6"><span class="d-flex align-items-center"><i class="fi-rr-marker mr-5 ml-0"></i><span class="font-sm color-text-mutted">Chicago, US</span></span></div>
-                                        <div class="col-6"><span class="d-flex justify-content-end align-items-center"><i class="fi-rr-clock mr-5"></i><span class="font-sm color-brand-1">$45 / hour</span></span></div>
                                     </div>
                                 </div>
                             </div>
