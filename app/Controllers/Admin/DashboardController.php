6ff55d183a9cf55910939ec15f360aa91c922473
nguyenhoangquoc2003 <118102345+nguyenhoangquoc2003@users.noreply.github.com>
2023-02-16 22:57:13 +0700

    dashboard (#52)
    * dashboard
    
    * dashboard

--- app/Controllers/Admin/DashboardController.php
+++ app/Controllers/Admin/DashboardController.php
@@ -5,6 +5,10 @@ declare(strict_types=1);
 namespace App\Controllers\Admin;
 
 use Core\Controller;
+use App\Models\Account;
+use App\Models\Company;
+use App\Models\Job;
+use App\Models\Application;
 
 class DashboardController extends Controller
 {
@@ -14,6 +18,11 @@ class DashboardController extends Controller
             redirect('admin/login');
         }
 
-        return view('admin/dashboard');
+        $accountsCount = Account::count();
+        $companiesCount = Company::count();
+        $jobsCount = Job::count();
+        $applicationsCount = Application::count();
+
+        return view('admin/dashboard', compact('accountsCount', 'companiesCount', 'jobsCount', 'applicationsCount'));
     }
 }
