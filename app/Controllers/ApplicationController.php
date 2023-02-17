8e6360769c65b5ab8f095be32aa55448dc517cc6
shibakunkun <nguyendangkhai111@gmail.com>
2023-02-17 23:39:03 +0700

    fix

--- app/Controllers/ApplicationController.php
+++ app/Controllers/ApplicationController.php
@@ -9,11 +9,15 @@ use App\Models\Application;
 
 class ApplicationController extends Controller
 {
-    public function store(){
+    public function store()
+    {
         $accountId = auth('account')->user()->id;
         $jobId = request()->input('job_id');
         
-        Application::create(['account_id' => $accountId , 'job_id' => $jobId]);
+        Application::create([
+            'account_id' => $accountId, 
+            'job_id' => $jobId,
+        ]);
         
         return redirect('account/applications/index');
     }
