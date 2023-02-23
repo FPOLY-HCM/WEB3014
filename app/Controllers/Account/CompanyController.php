84e14834d18a2f03a0d6da0f5eeb41aaefb45c8e
Khải <92205964+dang-khai@users.noreply.github.com>
2023-02-23 13:05:54 +0700

    edit delete account company (#71)

--- app/Controllers/Account/CompanyController.php
+++ app/Controllers/Account/CompanyController.php
@@ -29,7 +29,7 @@ class CompanyController extends Controller
         if (request()->has('logo')) {
             $logo = request()->file('logo')->upload('companies');
 
-            if (! $logo) {
+            if (!$logo) {
                 flash()->add('Có lỗi xảy ra khi tải ảnh lên.', 'danger');
 
                 return redirect('/account/companies/create');
@@ -43,5 +43,64 @@ class CompanyController extends Controller
         ]);
 
         return redirect('/account/companies');
+    }
+
+    public function edit()
+    {
+        $company = Company::findOrFail(request()->query('id'));
+
+        return view('account/companies/edit', compact('company'));
+    }
+
+    public function update()
+    {
+        if (
+            !request()->has('name')
+            || !request()->has('logo')
+            || !request()->has('email')
+            || !request()->has('phone')
+            || !request()->has('website')
+            || !request()->has('description')
+            || !request()->has('address')
+        ) {
+            flash()->add('Vui lòng nhập đầy đủ thông tin', 'danger');
+
+            return back();
+        }
+
+        $company = Company::findOrFail(request()->input('id'));
+        unlink($company->logo);
+        
+        if (request()->has('logo')) {
+            $logo = request()->file('logo')->upload('companies');
+
+            if (!$logo) {
+                flash()->add('Có lỗi xảy ra khi tải ảnh lên.', 'danger');
+
+                return redirect('/account/companies/update');
+            }
+        }
+
+        $company->update([
+            ...request()->input(),
+            'logo' => $logo,
+        ]);
+
+        flash()->add('Sửa thành công', 'success');
+
+        return redirect('/account/companies');
+    }
+
+    public function destroy()
+    {
+        $company = Company::findOrFail(request()->query('id'));
+
+        $company->delete();
+
+        unlink($company->logo);
+
+        flash()->add('Xóa thành công', 'success');
+
+        return back();
     }
 }
